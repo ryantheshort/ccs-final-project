5912633ce8776aa60a04ab931f1086a1d6263500
Ryan Short <shorttryan@gmail.com>
2022-10-31 14:48:12 -0400

    adds folder locations for app components and user stories

---
+++ frontend/static/src/components/App/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>Nav Featuring FC logo</div>
+    
+
+    <div>START SCORECARD</div>
+
+    <div>NEARBY COURSES</div>
+
+    <div>BLANK BUTTON</div>
+  );
+}
+
+export default App;
