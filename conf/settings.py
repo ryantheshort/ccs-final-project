7d139897939bf3b3cad2d87e0cf2eb3b0cd6a156
Ryan Short <shorttryan@gmail.com>
2022-11-01 10:33:57 -0400

    adds api app

--- conf/settings.py
+++ conf/settings.py
@@ -55,8 +55,19 @@ INSTALLED_APPS = [
     # Local
     'accounts.apps.AccountsConfig',
     'frontend.apps.FrontendConfig',
+    'api.apps.ApiConfig',
     
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
