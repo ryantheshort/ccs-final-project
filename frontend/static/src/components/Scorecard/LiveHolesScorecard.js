11af990fca622c1757ffbe38ef4eff4a0d0eb776
Ryan Short <shorttryan@gmail.com>
2022-11-09 11:01:50 -0500

    adds all 18 holes to live holes scorecard

--- frontend/static/src/components/Scorecard/LiveHolesScorecard.js
+++ frontend/static/src/components/Scorecard/LiveHolesScorecard.js
@@ -1,51 +1,257 @@
 import React, { useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
+import Form from 'react-bootstrap/Form';
+import '../../styles/LiveHolesScorecard.css';
+import Cookies from 'js-cookie';
+import { handleError } from "../../utils/helpers";
 
-function LiveHolesScorecard() {
+function LiveHolesScorecard(props) {
   const [index, setIndex] = useState(0);
-
+ 
   const handleSelect = (selectedIndex, e) => {
     setIndex(selectedIndex);
   };
 
+  const { userDetails, setUserDetails } = props;
+  // const [sidebar, setSidebar] = useState(false);
+  const [show, setShow] = useState(false);
+
+
+
+  // Toggles the navigation menu
+  // const showSidebar = () => setSidebar(!sidebar);
+
+  // Locks scrolling when the navigation menu is active
+
+
+    const handleLogout = async (e) => {
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-CSRFToken": Cookies.get("csrftoken"),
+        },
+      };
+      
+      const response = await fetch("/dj-rest-auth/logout/", options).catch(handleError);
+      if (!response.ok) {
+        throw new Error("Network response was not OK");
+      } else {
+        const data = await response.json();
+        Cookies.remove("Authorization");
+        localStorage.removeItem('username');
+        setUserDetails({
+          isAuth: false,
+          username: null
+        });
+       
+      }
+  
+    };
+
   return (
-    <Carousel activeIndex={index} interval={null} >
+    <Carousel activeIndex={index} interval={null} variant="dark" onSelect={handleSelect} >
+{/* HOLE 1 */}
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=First slide&bg=373940"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>Hole {index}</h3>
-          <p>Par <input type="text" id="par" name="name" required min="3" max="5"></input></p>
-        </Carousel.Caption>
+        <Form>
+          <Form.Group className="md-4" controlId={index}>
+            <h2>Hole {index + 1}</h2>
+            <p>Par <input type="number" id="par" name="par-number" required min="3" max="5"></input></p>
+            <p> <input type="number" id="distance" name="hole-distance" required></input>ft</p>
+            <h2 className="username">{userDetails && userDetails.username}</h2>
+          </Form.Group>
+        </Form>
       </Carousel.Item>
+{/* HOLE 2 */}
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Second slide&bg=282c34"
-          alt="Second slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
+        <Form>
+          <Form.Group className="md-4" controlId={index}>
+            <h2>Hole {index + 1}</h2>
+            <p>Par <input type="number" id="par" name="par-number" required min="3" max="5"></input></p>
+            <p> <input type="number" id="distance" name="hole-distance" required></input>ft</p>
+            <h2 className="username">{userDetails && userDetails.username}</h2>
+          </Form.Group>
+          </Form>
       </Carousel.Item>
+{/* HOLE 3 */}
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Third slide&bg=20232a"
-          alt="Third slide"
-        />
+        <Form>
+          <Form.Group className="md-4" controlId={index}>
+            <h2>Hole {index + 1}</h2>
+            <p>Par <input type="number" id="par" name="par-number" required min="3" max="5"></input></p>
+            <p> <input type="number" id="distance" name="hole-distance" required></input>ft</p>
+            <h2 className="username">{userDetails && userDetails.username}</h2>
+          </Form.Group>
+        </Form>
+      </Carousel.Item>
+{/* HOLE 4 */}
+<Carousel.Item>
+        <Form>
+          <Form.Group className="md-4" controlId={index}>
+            <h2>Hole {index + 1}</h2>
+            <p>Par <input type="number" id="par" name="par-number" required min="3" max="5"></input></p>
+            <p> <input type="number" id="distance" name="hole-distance" required></input>ft</p>
+            <h2 className="username">{userDetails && userDetails.username}</h2>
+          </Form.Group>
+        </Form>
+      </Carousel.Item>
+{/* HOLE 4 */}
+<Carousel.Item>
+        <Form>
+          <Form.Group className="md-4" controlId={index}>
+            <h2>Hole {index + 1}</h2>
+            <p>Par <input type="number" id="par" name="par-number" required min="3" max="5"></input></p>
+            <p> <input type="number" id="distance" name="hole-distance" required></input>ft</p>
+            <h2 className="username">{userDetails && userDetails.username}</h2>
+          </Form.Group>
+        </Form>
+      </Carousel.Item>
+{/* HOLE 5 */}
+<Carousel.Item>
+        <Form>
+          <Form.Group className="md-4" controlId={index}>
+            <h2>Hole {index + 1}</h2>
+            <p>Par <input type="number" id="par" name="par-number" required min="3" max="5"></input></p>
+            <p> <input type="number" id="distance" name="hole-distance" required></input>ft</p>
+            <h2 className="username">{userDetails && userDetails.username}</h2>
+          </Form.Group>
+        </Form>
+      </Carousel.Item>
+{/* HOLE 6 */}
+<Carousel.Item>
+        <Form>
+          <Form.Group className="md-4" controlId={index}>
+            <h2>Hole {index + 1}</h2>
+            <p>Par <input type="number" id="par" name="par-number" required min="3" max="5"></input></p>
+            <p> <input type="number" id="distance" name="hole-distance" required></input>ft</p>
+            <h2 className="username">{userDetails && userDetails.username}</h2>
+          </Form.Group>
+        </Form>
+      </Carousel.Item>
+{/* HOLE 7 */}
+<Carousel.Item>
+        <Form>
+          <Form.Group className="md-4" controlId={index}>
+            <h2>Hole {index + 1}</h2>
+            <p>Par <input type="number" id="par" name="par-number" required min="3" max="5"></input></p>
+            <p> <input type="number" id="distance" name="hole-distance" required></input>ft</p>
+            <h2 className="username">{userDetails && userDetails.username}</h2>
+          </Form.Group>
+        </Form>
+      </Carousel.Item>
+{/* HOLE 8 */}
+<Carousel.Item>
+        <Form>
+          <Form.Group className="md-4" controlId={index}>
+            <h2>Hole {index + 1}</h2>
+            <p>Par <input type="number" id="par" name="par-number" required min="3" max="5"></input></p>
+            <p> <input type="number" id="distance" name="hole-distance" required></input>ft</p>
+            <h2 className="username">{userDetails && userDetails.username}</h2>
+          </Form.Group>
+        </Form>
+      </Carousel.Item>
+{/* HOLE 9 */}
+<Carousel.Item>
+        <Form>
+          <Form.Group className="md-4" controlId={index}>
+            <h2>Hole {index + 1}</h2>
+            <p>Par <input type="number" id="par" name="par-number" required min="3" max="5"></input></p>
+            <p> <input type="number" id="distance" name="hole-distance" required></input>ft</p>
+            <h2 className="username">{userDetails && userDetails.username}</h2>
+          </Form.Group>
+        </Form>
+      </Carousel.Item>
+{/* HOLE 10 */}
+<Carousel.Item>
+        <Form>
+          <Form.Group className="md-4" controlId={index}>
+            <h2>Hole {index + 1}</h2>
+            <p>Par <input type="number" id="par" name="par-number" required min="3" max="5"></input></p>
+            <p> <input type="number" id="distance" name="hole-distance" required></input>ft</p>
+            <h2 className="username">{userDetails && userDetails.username}</h2>
+          </Form.Group>
+        </Form>
+      </Carousel.Item>
+{/* HOLE 11 */}
+<Carousel.Item>
+        <Form>
+          <Form.Group className="md-4" controlId={index}>
+            <h2>Hole {index + 1}</h2>
+            <p>Par <input type="number" id="par" name="par-number" required min="3" max="5"></input></p>
+            <p> <input type="number" id="distance" name="hole-distance" required></input>ft</p>
+            <h2 className="username">{userDetails && userDetails.username}</h2>
+          </Form.Group>
+        </Form>
+      </Carousel.Item>
+{/* HOLE 12 */}
+<Carousel.Item>
+        <Form>
+          <Form.Group className="md-4" controlId={index}>
+            <h2>Hole {index + 1}</h2>
+            <p>Par <input type="number" id="par" name="par-number" required min="3" max="5"></input></p>
+            <p> <input type="number" id="distance" name="hole-distance" required></input>ft</p>
+            <h2 className="username">{userDetails && userDetails.username}</h2>
+          </Form.Group>
+        </Form>
+      </Carousel.Item>
+{/* HOLE 13 */}
+<Carousel.Item>
+        <Form>
+          <Form.Group className="md-4" controlId={index}>
+            <h2>Hole {index + 1}</h2>
+            <p>Par <input type="number" id="par" name="par-number" required min="3" max="5"></input></p>
+            <p> <input type="number" id="distance" name="hole-distance" required></input>ft</p>
+            <h2 className="username">{userDetails && userDetails.username}</h2>
+          </Form.Group>
+        </Form>
+      </Carousel.Item>
+{/* HOLE 14 */}
+<Carousel.Item>
+        <Form>
+          <Form.Group className="md-4" controlId={index}>
+            <h2>Hole {index + 1}</h2>
+            <p>Par <input type="number" id="par" name="par-number" required min="3" max="5"></input></p>
+            <p> <input type="number" id="distance" name="hole-distance" required></input>ft</p>
+            <h2 className="username">{userDetails && userDetails.username}</h2>
+          </Form.Group>
+        </Form>
+      </Carousel.Item>
+{/* HOLE 15 */}
+<Carousel.Item>
+        <Form>
+          <Form.Group className="md-4" controlId={index}>
+            <h2>Hole {index + 1}</h2>
+            <p>Par <input type="number" id="par" name="par-number" required min="3" max="5"></input></p>
+            <p> <input type="number" id="distance" name="hole-distance" required></input>ft</p>
+            <h2 className="username">{userDetails && userDetails.username}</h2>
+          </Form.Group>
+        </Form>
+      </Carousel.Item>
+{/* HOLE 16 */}
+<Carousel.Item>
+        <Form>
+          <Form.Group className="md-4" controlId={index}>
+            <h2>Hole {index + 1}</h2>
+            <p>Par <input type="number" id="par" name="par-number" required min="3" max="5"></input></p>
+            <p> <input type="number" id="distance" name="hole-distance" required></input>ft</p>
+            <h2 className="username">{userDetails && userDetails.username}</h2>
+          </Form.Group>
+        </Form>
+      </Carousel.Item>
+{/* HOLE 17 */}
+<Carousel.Item>
+        <Form>
+          <Form.Group className="md-4" controlId={index}>
+            <h2>Hole {index + 1}</h2>
+            <p>Par <input type="number" id="par" name="par-number" required min="3" max="5"></input></p>
+            <p> <input type="number" id="distance" name="hole-distance" required></input>ft</p>
+            <h2 className="username">{userDetails && userDetails.username}</h2>
+          </Form.Group>
+        </Form>
+      </Carousel.Item>
+{/* TOTAL SCORECARD */}
+<Carousel.Item>
 
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
   );
